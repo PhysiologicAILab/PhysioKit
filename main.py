1d1007ae363cdd76ef581b890741028a3ef8ac02
jnj256 <jnj256@gmail.com>
2023-01-23 17:34:58 +0000

    Optimizing UI and data saving

--- main.py
+++ main.py
@@ -21,23 +21,15 @@ from matplotlib.figure import Figure
 
 import os
 import numpy as np
+import csv
 
 from devices import serialPort
 from datetime import datetime
-import calendar
+import calendar 
 
-global raw_ppg_signal_1, raw_ppg_signal_2
-global raw_resp, raw_eda
-global utc_time_stamp, arduino_time
+global live_acquisition_flag
 live_acquisition_flag = False
-update_bar_plot_axis = False
 
-raw_resp = []
-raw_eda = []
-raw_ppg_signal_1 = []
-raw_ppg_signal_2 = []
-utc_time_stamp = []
-arduino_time = []
 
 class InputDialog(QDialog):
     def __init__(self, parent=None):
@@ -70,8 +62,6 @@ class PPG(QWidget):
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         self.ui = loader.load(ui_file, self)
-        global raw_ppg_signal_1, raw_ppg_signal_2
-        global raw_resp, raw_eda
 
         self.ui.baudrate = 2000000
         self.ui.spObj = serialPort(self.ui.baudrate )
@@ -96,16 +86,15 @@ class PPG(QWidget):
         self.ui.comboBox_expName.currentIndexChanged.connect(self.update_expName)
         self.ui.pushButton_addExp.pressed.connect(self.add_exp)
 
+        self.ui.pid = ""
+        self.ui.lineEdit_PID.textChanged.connect(self.update_pid)
+
         self.ui.data_record_flag = False
         self.ui.data_root_dir = os.path.join(os.getcwd(), 'data')
         if not os.path.exists(self.ui.data_root_dir):
             os.makedirs(self.ui.data_root_dir)
+        
         self.ui.pushButton_record_data.pressed.connect(self.record_data)
-        raw_ppg_signal_1 = []
-        raw_ppg_signal_2 = []
-        raw_resp = []
-        raw_eda = []
-
         self.ui.exp_names = [self.ui.comboBox_expName.itemText(i) for i in range(self.ui.comboBox_expName.count())]
         self.ui.utc_timestamp_featDict = datetime.utcnow()
 
@@ -122,7 +111,6 @@ class PPG(QWidget):
         self.ui.graphicsView.setScene(self.graphic_scene)
         self.ui.graphicsView.show()
 
-
         # Add the callbackfunc
         self.ppgDataLoop = threading.Thread(name='ppgDataLoop', target=ppgDataSendLoop, daemon=True, args=(
             self.addData_callbackFunc, self.ui.spObj))
@@ -134,7 +122,11 @@ class PPG(QWidget):
 
     def addData_callbackFunc(self, value):
         # print("Add data: " + str(value))
-        self.myFig.addData(value)      
+        self.myFig.addData(value)
+        
+        if self.ui.data_record_flag:
+            eda_val, resp_val, ppg1_val, ppg2_val, ts_val = value
+            self.writer.writerow([eda_val, resp_val, ppg1_val, ppg2_val, ts_val])
         return
 
     def add_exp(self):
@@ -160,6 +152,10 @@ class PPG(QWidget):
         # self.ui.conditions = [self.ui.listWidget_expConditions.item(x).text() for x in range(self.ui.listWidget_expConditions.count())]
         self.ui.conditions = self.ui.exp_conds_dict[self.ui.curr_exp_name]
         self.ui.curr_exp_condition = self.ui.conditions[0]
+
+
+    def update_pid(self, text):
+        self.ui.pid = text
 
     def update_serial_port(self):
         self.ui.curr_ser_port_name = self.ui.ser_port_names[self.ui.comboBox_comport.currentIndex()]
@@ -217,63 +213,31 @@ class PPG(QWidget):
         self.ui.label_status.setText("Experiment Condition Selected: " + self.ui.curr_exp_condition)
 
     def record_data(self):
+
         if not self.ui.data_record_flag:
             self.ui.data_record_flag = True
-            self.ui.utc_timestamp_signal = datetime.utcnow()
+
+            utc_sec = str((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds())
+            utc_sec = utc_sec.replace('.', '_')
+            self.save_file_path = os.path.join(self.ui.data_root_dir, self.ui.pid + "_" +  self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' + utc_sec + '.csv')
+
+            self.csvfile = open(self.save_file_path, 'w', encoding="utf", newline="")
+            self.writer = csv.writer(self.csvfile)
+            self.writer.writerow(["eda", "resp", "ppg1", "ppg2", "arduino_ts"])
+
+            # self.ui.utc_timestamp_signal = datetime.utcnow()
             self.ui.pushButton_record_data.setText("Stop Recording")
             self.ui.label_status.setText("Recording started for: Exp - " + self.ui.curr_exp_name + "; Condition - " + self.ui.curr_exp_condition)
         else:
-            th = threading.Thread(target=self.save_raw_signal)
-            th.start()
+            self.csvfile.close()
             self.ui.data_record_flag = False
             self.ui.pushButton_record_data.setText("Start Recording")
             self.ui.label_status.setText("Recording stopped and data saved for: Exp - " + self.ui.curr_exp_name + "; Condition - " + self.ui.curr_exp_condition)
-
-    def save_raw_signal(self):
-        global raw_ppg_signal_1, raw_ppg_signal_2
-        global raw_resp, raw_eda
-        global utc_time_stamp, arduino_time
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                                    'raw_eda_' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(raw_eda))
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                                    'raw_resp_' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(raw_resp))
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                            'raw_ppg1_' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(raw_ppg_signal_1))
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                            'raw_ppg2_' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(raw_ppg_signal_2))
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                                    'time_stamp' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(utc_time_stamp))
-
-        fname_signal = os.path.join(self.ui.data_root_dir, self.ui.curr_exp_name + '_' + self.ui.curr_exp_condition + '_' +
-                                    'arduino_time' + str(calendar.timegm(self.ui.utc_timestamp_signal.timetuple())) + '.npy')
-        np.save(fname_signal, np.array(arduino_time))
-
-        raw_resp = []
-        raw_eda = []
-        raw_ppg_signal_1 = []
-        raw_ppg_signal_2 = []
-        utc_time_stamp = []
-        arduino_time = []
-
 
 
 class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
     def __init__(self, uiObj):
         self.uiObj = uiObj
-        self.added_ppg1_data = []
-        self.added_ppg2_data = []
-        self.added_resp_data = []
-        self.added_eda_data = []
         self.exception_count = 0
         # print(matplotlib.__version__)
         # The data
@@ -288,18 +252,15 @@ class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
         self.eda_plot_signal = (self.x_axis * 0.0) + 50
         self.x_axis = self.x_axis/self.uiObj.fs
         # The window
-        self.fig = Figure(figsize=(22,14), dpi=50, tight_layout=True)
+        self.fig = Figure(figsize=(16,12), dpi=75, tight_layout=True)
+        # self.fig = Figure(tight_layout=True)
 
         self.ax1 = self.fig.add_subplot(3, 1, 1)
         # self.ax1 settings
         self.ax1.set_xlabel('Time (seconds)', fontsize=24)
         self.ax1.set_ylabel('PPG Signal', fontsize=24)
         self.line1 = Line2D([], [], color='blue')
-        self.line1_tail = Line2D([], [], color='red', linewidth=3)
-        self.line1_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
         self.ax1.add_line(self.line1)
-        self.ax1.add_line(self.line1_tail)
-        self.ax1.add_line(self.line1_head)
         self.ax1.set_xlim(0, self.max_time)
         # self.ax1.autoscale(enable=True, axis='y', tight=True)
         self.ax1.set_ylim(-100, 200)
@@ -318,11 +279,7 @@ class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
         self.ax2.set_xlabel('Time (seconds)', fontsize=24)
         self.ax2.set_ylabel('Respiratory Signal', fontsize=24)
         self.line2 = Line2D([], [], color='blue')
-        self.line2_tail = Line2D([], [], color='red', linewidth=3)
-        self.line2_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
         self.ax2.add_line(self.line2)
-        self.ax2.add_line(self.line2_tail)
-        self.ax2.add_line(self.line2_head)
         self.ax2.set_xlim(0, self.max_time)
         # self.ax2.autoscale(enable=True, axis='y', tight=True)
         self.ax2.set_ylim(-100, 200)
@@ -341,11 +298,7 @@ class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
         self.ax3.set_xlabel('Time (seconds)', fontsize=24)
         self.ax3.set_ylabel('Electrodermal Activity Signal', fontsize=24)
         self.line3 = Line2D([], [], color='blue')
-        self.line3_tail = Line2D([], [], color='red', linewidth=3)
-        self.line3_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
         self.ax3.add_line(self.line3)
-        self.ax3.add_line(self.line3_tail)
-        self.ax3.add_line(self.line3_head)
         self.ax3.set_xlim(0, self.max_time)
         # self.ax3.autoscale(enable=True, axis='y', tight=True)
         self.ax3.set_ylim(-100, 200)
@@ -371,41 +324,37 @@ class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
         self.ppg2_filt_obj = lFilter(ppg_lowcut, ppg_highcut, self.uiObj.fs, order=filt_order)
         self.resp_filt_obj = lFilter(resp_lowcut, resp_highcut, self.uiObj.fs, order=filt_order)
         self.eda_filt_obj = lFilter_moving_average(window_size=moving_average_window_size)
-        self.count_frame = 0# self.max_time * self.uiObj.fs
+        self.count_frame = 0 # self.max_time * self.uiObj.fs
         return
 
     def new_frame_seq(self):
         return iter(range(self.x_axis.size))
 
     def _init_draw(self):
-        lines = [self.line1, self.line1_tail, self.line1_head, self.line2,
-                     self.line2_tail, self.line2_head, self.line3, self.line3_tail, self.line3_head]
+        lines = [self.line1, self.line2, self.line3]
         for l in lines:
             l.set_data([], [])
         return
 
     def addData(self, value):
-        global raw_resp, raw_eda
-        global raw_ppg_signal_1, raw_ppg_signal_2
-        global utc_time_stamp, arduino_time
-        eda_val, resp_val, ppg1_val, ppg2_val, ts_val = value
+        self.count_frame += 1
+        eda_val, resp_val, ppg1_val, ppg2_val, _ = value
         ppg1_filtered = self.ppg1_filt_obj.lfilt(ppg1_val)
         ppg2_filtered = self.ppg2_filt_obj.lfilt(ppg2_val)
         resp_filtered = self.resp_filt_obj.lfilt(resp_val)
         eda_filtered = self.eda_filt_obj.lfilt(eda_val)
-        # self.added_ppg1_data.append(value)
-        self.added_ppg1_data.append(ppg1_filtered)
-        self.added_ppg2_data.append(ppg2_filtered)
-        self.added_resp_data.append(resp_filtered)
-        self.added_eda_data.append(eda_filtered)
-        if self.uiObj.data_record_flag:
-            raw_eda.append(eda_val) # raw signals saved without any filtering.
-            raw_resp.append(resp_val)
-            raw_ppg_signal_1.append(ppg1_val)
-            raw_ppg_signal_2.append(ppg2_val) # raw signals saved without any filtering.
-            utc_sec = (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
-            utc_time_stamp.append(utc_sec)
-            arduino_time.append(ts_val)
+
+        self.eda_plot_signal = np.roll(self.eda_plot_signal, -1)
+        self.eda_plot_signal[-1] = eda_filtered
+
+        self.resp_plot_signal = np.roll(self.resp_plot_signal, -1)
+        self.resp_plot_signal[-1] = resp_filtered
+
+        self.ppg1_plot_signal = np.roll(self.ppg1_plot_signal, -1)
+        self.ppg1_plot_signal[-1] = ppg1_filtered
+
+        self.ppg2_plot_signal = np.roll(self.ppg2_plot_signal, -1)
+        self.ppg2_plot_signal[-1] = ppg2_filtered
 
         return
 
@@ -424,64 +373,22 @@ class LivePlotFigCanvas(FigureCanvas, TimedAnimation):
     def _draw_frame(self, framedata):
         global live_acquisition_flag
         if live_acquisition_flag:   
-            margin = 2
-            while(len(self.added_ppg1_data) > 0):
-
-                self.ppg1_plot_signal = np.roll(self.ppg1_plot_signal, -1)
-                self.ppg1_plot_signal[-1] = self.added_ppg1_data[-1]
-                del(self.added_ppg1_data[0])
-
-                self.ppg2_plot_signal = np.roll(self.ppg2_plot_signal, -1)
-                self.ppg2_plot_signal[-1] = self.added_ppg2_data[-1]
-                del(self.added_ppg2_data[0])
-
-                self.resp_plot_signal = np.roll(self.resp_plot_signal, -1)
-                self.resp_plot_signal[-1] = self.added_resp_data[-1]
-                del(self.added_resp_data[0])
-
-                self.eda_plot_signal = np.roll(self.eda_plot_signal, -1)
-                self.eda_plot_signal[-1] = self.added_eda_data[-1]
-                del(self.added_eda_data[0])
-
-                self.count_frame += 1
 
             if self.count_frame >= (self.measure_time * self.uiObj.fs):
                 self.count_frame = 0
                 # self.ax1.autoscale(axis='y', tight=True)
                 # self.ax2.autoscale(axis='y', tight=True)
                 # self.ax3.autoscale(axis='y', tight=True)
-                self.ax1.set_ylim(np.min(self.ppg1_plot_signal[-self.normalizing_time*self.uiObj.fs:]), np.max(
-                    self.ppg1_plot_signal[-self.normalizing_time*self.uiObj.fs:]))
-                
-                self.ax2.set_ylim(np.min(self.resp_plot_signal[-self.normalizing_time*self.uiObj.fs:]), np.max(
-                    self.resp_plot_signal[-self.normalizing_time*self.uiObj.fs:]))
 
-                self.ax3.set_ylim(np.min(self.eda_plot_signal[-self.normalizing_time*self.uiObj.fs:]), np.max(
-                    self.eda_plot_signal[-self.normalizing_time*self.uiObj.fs:]))
+                self.ax1.set_ylim(np.min(self.ppg1_plot_signal), np.max(self.ppg1_plot_signal))
+                self.ax2.set_ylim(np.min(self.resp_plot_signal), np.max(self.resp_plot_signal))
+                self.ax3.set_ylim(np.min(self.eda_plot_signal), np.max(self.eda_plot_signal))
 
-            self.line1.set_data(self.x_axis[0: self.x_axis.size - margin],
-                                self.ppg1_plot_signal[0: self.x_axis.size - margin])
-            self.line1_tail.set_data(np.append(self.x_axis[-10:-1 - margin], self.x_axis[-1 - margin]), np.append(
-                self.ppg1_plot_signal[-10:-1 - margin], self.ppg1_plot_signal[-1 - margin]))
-            self.line1_head.set_data(self.x_axis[-1 - margin], self.ppg1_plot_signal[-1 - margin])
+            self.line1.set_ydata(self.ppg1_plot_signal)
+            self.line2.set_ydata(self.resp_plot_signal)
+            self.line3.set_ydata(self.eda_plot_signal)
 
-            self.line2.set_data(self.x_axis[0: self.x_axis.size - margin],
-                                self.resp_plot_signal[0: self.x_axis.size - margin])
-            self.line2_tail.set_data(np.append(self.x_axis[-10:-1 - margin], self.x_axis[-1 - margin]), np.append(
-                self.resp_plot_signal[-10:-1 - margin], self.resp_plot_signal[-1 - margin]))
-            self.line2_head.set_data(
-                self.x_axis[-1 - margin], self.resp_plot_signal[-1 - margin])
-
-            self.line3.set_data(self.x_axis[0: self.x_axis.size - margin],
-                                self.eda_plot_signal[0: self.x_axis.size - margin])
-            self.line3_tail.set_data(np.append(self.x_axis[-10:-1 - margin], self.x_axis[-1 - margin]), np.append(
-                self.eda_plot_signal[-10:-1 - margin], self.eda_plot_signal[-1 - margin]))
-            self.line3_head.set_data(
-                self.x_axis[-1 - margin], self.eda_plot_signal[-1 - margin])
-
-            self._drawn_artists = [self.line1, self.line1_tail, self.line1_head, self.line2,
-                                   self.line2_tail, self.line2_head, self.line3, self.line3_tail, self.line3_head]
-
+            self._drawn_artists = [self.line1, self.line2, self.line3, ]
 
         return
 
