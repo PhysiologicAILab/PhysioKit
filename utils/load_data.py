a88bf76f965f1c05911f36d4f2cac4c3bb2f7f41
jnj256 <jnj256@gmail.com>
2023-01-31 00:48:05 +0000

    minor updates in loading data for analysis

--- utils/load_data.py
+++ utils/load_data.py
@@ -2,8 +2,8 @@ import csv
 import numpy as np
 
 def load_numpy_data(filepath):
-    sig = np.load(filepath)
-    return sig
+    data = np.load(filepath)
+    return data
 
 def load_csv_data_ppg(filepath):
     ppg1 = []
@@ -35,3 +35,62 @@ def load_csv_data_ppg(filepath):
     event_code = np.array(event_code)
 
     return ppg1, ppg2, arduino_ts, event_code
+
+
+def load_csv_data_eda(filepath):
+    eda = []
+    arduino_ts = []
+    event_code = []
+    skip_first = True
+    with open(filepath, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for ln in csvreader:
+            if skip_first:
+                print(ln)
+                skip_first = False
+            else:
+                # print(ln)
+                # break
+                eda.append(float(ln[0]))
+                arduino_ts.append(float(ln[4]))
+                if ln[5] != '':
+                    event_code.append(float(ln[5]))
+                else:
+                    event_code.append(-1)
+
+    eda = np.array(eda)
+    arduino_ts = np.array(arduino_ts)
+    arduino_ts = (arduino_ts - arduino_ts[0])/1000
+    event_code = np.array(event_code)
+
+    return eda, arduino_ts, event_code
+
+
+def load_csv_data_resp(filepath):
+    resp = []
+    arduino_ts = []
+    event_code = []
+    skip_first = True
+    with open(filepath, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for ln in csvreader:
+            if skip_first:
+                print(ln)
+                skip_first = False
+            else:
+                # print(ln)
+                # break
+                resp.append(float(ln[1]))
+                arduino_ts.append(float(ln[4]))
+                if ln[5] != '':
+                    event_code.append(float(ln[5]))
+                else:
+                    event_code.append(-1)
+
+    resp = np.array(resp)
+    arduino_ts = np.array(arduino_ts)
+    arduino_ts = (arduino_ts - arduino_ts[0])/1000
+    event_code = np.array(event_code)
+
+    return resp, arduino_ts, event_code
+
